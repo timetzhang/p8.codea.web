61e5beab65599da9d3a7f2f88f3bc8ec8bf4af13
hjc <hjcshaqiu@163.com>
2017-07-19 10:49:57 +0800

    10:49

--- src/common/config.js
+++ src/common/config.js
@@ -1,7 +1,6 @@
 // config.js
 
 export default {
-    //dbBaseUrl: 'http://codeadb.joinp8.com',
-    dbBaseUrl: 'http://localhost:8006',
+    dbBaseUrl: 'http://codeadb.joinp8.com',
     title: 'CodeA - P8'
 };
